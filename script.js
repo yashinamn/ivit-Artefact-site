5f3a37a32080f7d8e466310813d5c925003bda14
yashinamn <enigm@list.ru>
2020-04-12 19:31:28 +0300

    1) анимацию бургера, именно открытие и закрытие хотелось бы чуть плавнее - сделала 2) на странице Частые вопросы, ответы на вопросы должны раскрываться при клике, а не при наведении - сделала
    4) в портфолио не работает галерея, картинки в попапе не листаются - добавила слайдер, он конечно пока корявый, я только сегодня научилась его делать. позже докручу. но сейчас уже листает.
    5) у проектов нет в попапе описания - пока не знаю как это сделать, может есть подсказка?
    6) в адаптиве полупрозрачный фон у галереи не закрывает весь экран, т.е. появляется галерея на темном фоне в попапе,   но если проскроллить страницу ниже, то дальше под ней виден сайт - исправила

--- script.js
+++ script.js
@@ -11,3 +11,37 @@ $(document).ready(function () {
         $('.phoneButton, .buttonsWrap').toggleClass('active');
     });
 });
+
+
+$(document).ready(function(){
+    $('.slider').slick({
+        asNavFor:'.sliderMin',
+        arrows:true,
+        dots:false,
+        adaptiveHeight:true,
+        slidesToShow:1,
+        slidesToScroll:1,
+        speed:500,
+        easing:'ease',
+        infinite:true,
+        initialSlide:0,
+        autoplay:false,
+        fade:true,   
+    });
+    $('.sliderMin').slick({
+        asNavFor:'.slider',
+        arrows:false,
+        dots:false,
+        adaptiveHeight:true,
+        adaptiveWidth:true,
+        slidesToShow:3,
+        slidesToScroll:1,
+        speed:500,
+        easing:'ease',
+        infinite:true,
+        initialSlide:0,
+        autoplay:false,
+        vertical:true,
+        verticalSwiping:true,
+    })
+});
